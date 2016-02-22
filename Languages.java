bbfa82bca0c72e1a906d8eafa0262850fa427d8a
Chris <c.davietei@gmail.com>
2016-02-22 01:31:17 -0500

    Added: 	ENGLISH_US.xml 	Languages.java 	LocalizedStrings.java 	TextFields.java
    Initial commit of localization support and a class that will allow all labels within the GUI window of the application to remain consistent throughout the interface

---
+++ Languages.java
@@ -0,0 +1,14 @@
+/**
+ * Contains all the available languages that the application supports
+ *
+ * @author Christopher Davie
+ * @version 1.0
+ */
+public enum Languages
+{
+	/**
+	 * American English
+	 * @since 1.0
+	 */
+	ENGLISH_US
+}//Languages enum
