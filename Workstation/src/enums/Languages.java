271beadc202b4ed86309ca342b5ad6862ad9fd39
Chris <c.davietei@gmail.com>
2016-03-17 18:01:57 -0400

    Updated File structure

---
+++ Workstation/src/enums/Languages.java
@@ -0,0 +1,20 @@
+package enums;
+
+
+
+/**
+ * Contains all the available languages that the application supports
+ *
+ * @author Christopher Davie
+ * @version 1.0
+ */
+public enum Languages
+{
+	/**
+	 * American English
+	 * @since 1.0
+	 */
+	ENGLISH_US,
+
+	SPANISH
+}//Languages enum
---
+++ Workstation/src/util/Localized.java
@@ -0,0 +1,22 @@
+package util;
+
+/**
+ * This interface defines a method to be implemented in any GUI window to allow
+ * the main program controller to update the UI labels and text fields in the
+ * event that the application's global language is changed. To be used with
+ * {@link LocalizedStrings}.
+ *
+ * @author Christopher Davie
+ * @version 1.0
+ */
+public interface Localized
+{
+    /**
+     * Updates all the labels and text fields present within a GUI window to
+     * the correct language in the event the language has changed
+     * @return  True if labels updated successfully and false if not
+     * @since  1.0
+     */
+    public boolean updateLabels();
+
+}//Localized interface
