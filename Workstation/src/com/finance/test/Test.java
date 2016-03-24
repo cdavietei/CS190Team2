8ab462fafd328fbf430aaf21e81ea0a5137b4e11
Chris <c.davietei@gmail.com>
2016-03-24 17:42:07 -0400

    Stock Interface changed

--- Workstation/src/com/finance/test/Test.java
+++ Workstation/src/com/finance/test/Test.java
@@ -1,24 +1,41 @@
 package com.finance.test;
 
 import java.io.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.*;
+
+import com.finance.main.java.database.SQLiteConnector;
+import com.finance.main.java.database.StockDatabaseInterface;
 import com.finance.main.java.enums.*;
+import com.finance.main.java.stock.Stock;
 import com.finance.main.java.util.*;
 
 public class Test
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		LocalizedStrings.setLanguage(Languages.ENGLISH_US);
-
-		for(TextFields i : TextFields.values())
-			System.out.printf("%s: %s\n",i.toString(),LocalizedStrings.getLocalString(i));
-
-		LocalizedStrings.setLanguage(Languages.SPANISH);
-		System.out.println();
-
-		for(TextFields i : TextFields.values())
-			System.out.printf("%s: %s\n",i.toString(),LocalizedStrings.getLocalString(i));
+		
+		
+		StockDatabaseInterface stocks = new StockDatabaseInterface();
+		
+		stocks.addNewStock("GOOG");
+		
+		ArrayList<Stock> list = stocks.getStocks("GOOG", new Date(System.currentTimeMillis() - (10 * 1000 * 60 * 60 * 24)), new Date(System.currentTimeMillis()));
+		
+		System.out.println(list);
+		
+		/*
+		SQLiteConnector con = new SQLiteConnector();
+		
+		ResultSet results = con.executeQuery("SELECT * FROM GOOG_info;");
+		System.out.println(results.getString("Date"));
+		
+		results.close();
+		con.close();
+		*/
 	}
 
 }
