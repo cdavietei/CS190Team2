c4cde94eaa2083501a1366a598fcdc6e9be05936
Mazharul Onim <mi.onim@hotmail.com>
2016-03-31 20:00:45 -0400

    Revert "HistTable now gets data for stocks and displays."
    This reverts commit 53b52ffa874f49a76bb6d0526b8fc7625d19633e.

--- Workstation/src/com/finance/main/java/histTable/HistTableView.java
+++ Workstation/src/com/finance/main/java/histTable/HistTableView.java
@@ -1,13 +1,7 @@
 package com.finance.main.java.histTable;
 
 import javax.swing.JPanel;
-import com.finance.main.java.stock.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.BorderFactory;
@@ -20,25 +14,17 @@ import javax.swing.JToolBar;
 import javax.swing.JScrollPane;
 import com.finance.main.java.enums.*;
 import com.finance.main.java.util.*;
-import com.finance.main.java.database.*;
 public class HistTableView extends JPanel {
 	
-	private HistTableSettings settingsFrame = new HistTableSettings();
+	private HistTableSettings settingsFrame; //= new HistTableSettings();
 	private JTable table;
-	public DefaultTableModel data;
+	DefaultTableModel data;
 	public JScrollPane scrollpane;
 	/**
 	 * Create the panel.
 	 */
-
 	Object[][] tableData = new Object[10][2];
 
-
-	public String[] columnNames = {LocalizedStrings.getLocalString(TextFields.TABLE_SYMBOL),
-		LocalizedStrings.getLocalString(TextFields.TABLE_DATE),LocalizedStrings.getLocalString(TextFields.TABLE_HIGH),
-		LocalizedStrings.getLocalString(TextFields.TABLE_LOW),LocalizedStrings.getLocalString(TextFields.TABLE_OPEN),
-		LocalizedStrings.getLocalString(TextFields.TABLE_CLOSE),LocalizedStrings.getLocalString(TextFields.TABLE_VOLUME),
-		LocalizedStrings.getLocalString(TextFields.TABLE_ADJCLOSE)};
 	JCheckBox[] checkBoxes={settingsFrame.symbolBox,settingsFrame.dateBox,
 			settingsFrame.highBox,settingsFrame.lowBox,settingsFrame.openBox,settingsFrame.closeBox,
 			settingsFrame.volumeBox,settingsFrame.adjCloseBox};
@@ -62,7 +48,6 @@ public class HistTableView extends JPanel {
 					updateTable();
 			}
 		});
-		
 		btnNewButton.setIcon(new ImageIcon("C:\\Users\\Carl\\Desktop\\settings1_16x16.gif"));
 		btnNewButton.setBounds(280, 0, 23, 23);
 		add(btnNewButton);
@@ -100,87 +85,23 @@ public class HistTableView extends JPanel {
 	/**
 	 * Updates a table
 	 */
-	public void updateTable(){
-		StockDatabaseInterface inter = new StockDatabaseInterface();
-		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-		java.util.Date date1 = new java.util.Date();
-		try {
-			date1 = formatter.parse("20/03/2016");
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		java.util.Date date2 = new java.util.Date();
-		try {
-			date2 = formatter.parse("30/03/2016");
-		} catch (ParseException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		ArrayList<Stock> stockArray = null;
-		try {
-			stockArray = inter.getStocks("GOOG", new Date(date1.getTime()), new Date(date2.getTime()));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		data = (DefaultTableModel) table.getModel();
-		for(int i = data.getRowCount() - 1; i >= 0; i--)
+	public void updateTable(){	
+		Object[][] columns = {{LocalizedStrings.getLocalString(TextFields.TABLE_SYMBOL),null},
+			{LocalizedStrings.getLocalString(TextFields.TABLE_DATE),null},
+			{LocalizedStrings.getLocalString(TextFields.TABLE_HIGH),null},
+			{LocalizedStrings.getLocalString(TextFields.TABLE_LOW), null},
+			{LocalizedStrings.getLocalString(TextFields.TABLE_OPEN),null},
+			{LocalizedStrings.getLocalString(TextFields.TABLE_CLOSE),null},
+			{LocalizedStrings.getLocalString(TextFields.TABLE_VOLUME),null},
+			{LocalizedStrings.getLocalString(TextFields.TABLE_ADJCLOSE),null}};
+		for(int i = data.getRowCount() -1; i >= 0; i--)
 			data.removeRow(i);
-		int colNeeded= 0;
 		for(int i = 0; i < checkBoxes.length; i++){
 			if(checkBoxes[i].isSelected()){
-				colNeeded++;
+				//data.addRow(rows[i]);
 			}
 		}//for
-		String[] columns = new String[colNeeded];
-		int colIndex = 0;
-		for(int i = 0; i < checkBoxes.length; i++){
-			if(checkBoxes[i].isSelected()){
-				columns[colIndex] = columnNames[i];
-				colIndex++;
-			}
-		}//for
-		Object[] row;
-		for(int i =0; i < stockArray.size(); i++){
-			row = new Object[colNeeded];
-			int index = 0;
-			if(checkBoxes[0].isSelected()){
-				row[index] = stockArray.get(i).getSymbol();
-				index++;
-			}
-			if(checkBoxes[1].isSelected()){
-				row[index] = stockArray.get(i).getDate();
-				index++;
-			}
-			if(checkBoxes[2].isSelected()){
-				row[index] = stockArray.get(i).getHigh();
-				index++;
-			}
-			if(checkBoxes[3].isSelected()){
-				row[index] = stockArray.get(i).getLow();
-				index++;
-			}
-			if(checkBoxes[4].isSelected()){
-				row[index] = stockArray.get(i).getOpen();
-				index++;
-			}
-			if(checkBoxes[5].isSelected()){
-				row[index] = stockArray.get(i).getClose();
-				index++;
-			}
-			if(checkBoxes[6].isSelected()){
-				row[index] = stockArray.get(i).getVolume();
-				index++;
-			}
-			if(checkBoxes[7].isSelected()){
-				row[index] = stockArray.get(i).getAdjClose();
-				index++;
-			}
-			data.addRow(row);
-				
-		}
-		
+		//data = new DefaultTableModel(rows,columns);
 		table.setModel(data);
 		settingsFrame.setVisible(false);
 		//scrollpane.setSize(table.getSize());
